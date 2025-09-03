179a3cc47550fc4e7ab62ccb0dc8857d4d8cf36b
Matthias Neeracher <microtherion@gmail.com>
2025-09-03 22:07:26 +0200

    Imgur is using response_type=token, and passes tokens directly in the callback URL; currently not testable because it's permanently 'over capacity'

--- Sources/ImperialImgur/ImgurRouter.swift
+++ Sources/ImperialImgur/ImgurRouter.swift
@@ -20,32 +20,18 @@ struct ImgurRouter: FederatedServiceRouter {
     }
 
     func authURL(_ request: Request) throws -> String {
-        return "https://api.imgur.com/oauth2/authorize?" + "client_id=\(self.tokens.clientID)&" + "response_type=code"
+        return "https://api.imgur.com/oauth2/authorize?" + "client_id=\(self.tokens.clientID)&" + "response_type=token"
     }
 
     func fetchToken(from request: Request) async throws -> String {
-        let code: String
-        if let queryCode: String = try request.query.get(at: codeKey) {
-            code = queryCode
-        } else if let error: String = try request.query.get(at: errorKey) {
-            throw Abort(.badRequest, reason: error)
-        } else {
-            throw Abort(.badRequest, reason: "Missing 'code' key in URL query")
-        }
-
-        let body = self.callbackBody(with: code)
-        let url = URI(string: accessTokenURL)
-
-        let buffer = try await body.encodeResponse(for: request).body.buffer
-        let response = try await request.client.post(url, headers: self.callbackHeaders) { $0.body = buffer }
-
-        let refresh = try response.content.get(String.self, at: ["refresh_token"])
+        let refresh = try? request.query.get(String.self, at: "refresh_token")
         request.session.setRefreshToken(refresh)
 
-        return try response.content.get(String.self, at: ["access_token"])
+        return try request.query.get(String.self, at: "access_token")
     }
 
     func callbackBody(with code: String) -> any AsyncResponseEncodable {
-        ImgurCallbackBody(code: code, clientId: self.tokens.clientID, clientSecret: self.tokens.clientSecret)
+        return Response(status: .badRequest)
     }
+
 }
